bbb041b3aefac147a665f83b55bf707ef009adab
hnaa0 <hnaa12@daum.net>
2023-05-18 14:36:49 +0900

    ⚙setup + LogIn form

---
+++ src/routes/Auth.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <button>Continue with Google</button>
+      <button>Continue with Github</button>
+    </div>
+  );
+}
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default EditProfile = () => {
+  <span>Edit Profile</span>;
+};
