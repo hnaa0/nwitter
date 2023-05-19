998d11a8745382df6c812b58f55e677b86b3d70b
hnaa0 <hnaa12@daum.net>
2023-05-19 12:02:33 +0900

    ⚙Database Setup + 📱Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,38 @@
-import React from "react";
+import { dbService } from "fBase";
+import React, { useState } from "react";
 
 export default function Home() {
-  return <span>Home</span>;
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createAt: Date.now(),
+    });
+    setNweet("");
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={nweet}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" maxLength={120} />
+      </form>
+    </div>
+  );
 }
